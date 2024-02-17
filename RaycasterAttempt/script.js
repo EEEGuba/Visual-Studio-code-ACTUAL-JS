0a79c63ed23846c3820f6c69eded415dee0f2f96
EEEGuba <grzes_pta@o2.pl>
2024-02-17 02:08:59 +0100

    b

--- RaycasterAttempt/script.js
+++ RaycasterAttempt/script.js
@@ -4,11 +4,13 @@ const fov = 30
 const fps = 40
 const renderaccuracy = 50 //ammount of blocks per frame
 const turnsensitivity = 6 //degrees turning on click of a or d
-const steplength = 1 //how far you go after w or s
+const steplength = 3 //how far you go after w or s
 const renderdistance = 200 //impacts how far away a wall has to be to not appear, much longer distances might slow down the game
 
 
 //end of settings
+
+let playerpos = { x: 250, y: 240, rotation: 90 }
 
 const myCanvas = document.getElementById("content");
 myCanvas.height = 500;
@@ -27,7 +29,12 @@ function keySwitchboard(event) {
     console.log(event.key)
     switch (event.key) {
         case "w":
-
+            console.log(playerpos)
+            const a = calculateVectorDisplacement(angleCorrector(playerpos.rotation), steplength)
+            playerpos.x += a.x
+            playerpos.y += a.y
+            drawPlayerOnMap()
+            testDraw()
             break;
 
         case "a":
@@ -39,23 +46,63 @@ function keySwitchboard(event) {
         case "d":
 
             break;
+        case "j":
+            playerpos.rotation -= turnsensitivity
+            playerpos.rotation = angleCorrector(playerpos.rotation)
+            break;
+        case "l":
+            playerpos.rotation += turnsensitivity
+            playerpos.rotation = angleCorrector(playerpos.rotation)
+            break;
         default:
             break;
     }
+    function angleCorrector(angle) {
+        if (angle > 359) { return (angle - 360) }
+        else if (angle < 0) { return (angle + 359) }
+        return (angle)
+    }
+
+}
+function testDraw() {
+    ctx.clearRect(0, 0, myCanvas.height, myCanvas.width)
+    const rayResult = rayCastingReturnWall(playerpos, playerpos.rotation, renderdistance)
+    if (rayResult == undefined) { return }
+    console.log("it went through")
+    ctx.fillStyle = rayResult.material
+    const wallProportions = Math.round(myCanvas.height / rayResult.length)
+    ctx.fillRect(myCanvas.width / 2 - 5, myCanvas.height - wallProportions, 10, wallProportions)
+}
+function drawPlayerOnMap() {
+    drawSquare(playerpos.x, playerpos.y, "magenta", 2, ctm)
 }
 function drawMap() {
     console.log(mapData)
     ctm.clearRect(0, 0, myMap.height, myMap.width)
-    for (let i = 0; i <= (mapData.length)-1; i++) {
-        console.log(mapData[0].length)
-        for (let j = 0; j <= (mapData[i].length)-1; j++) {
+    for (let i = 0; i <= (mapData.length) - 1; i++) {
+        for (let j = 0; j <= (mapData[i].length) - 1; j++) {
             const element = mapData[i][j];
             drawSquare(element.x, element.y, element.material, 1, ctm)
         };
     }
+    drawPlayerOnMap()
 }
-function returnVectorDisplacementByAngle() {
+function rayCastingReturnWall(startingPoint, angle, length) {
+    for (let k = 0; k < length; k++) {
+        const vectorDisplacement = calculateVectorDisplacement(angle, 1)
+        const currentPoint = { x: Math.floor(startingPoint.x + vectorDisplacement.x), y: Math.floor(startingPoint.y + vectorDisplacement.y) }
+        for (let i = 0; i <= (mapData.length) - 1; i++) {
+            for (let j = 0; j <= (mapData[i].length) - 1; j++) {
+                const element = mapData[i][j];
+                if (j == 100) { console.log(element.x, currentPoint.x, element.y, currentPoint.y) }
+                if (element.x == currentPoint.x && element.y == currentPoint.y) {
+                    return { x: element.x, y: element.y, cycleNumber: element.cycleNumber, material: element.material, rayLength: k }
+                }
+            }
 
+        }
+    }
+    return undefined
 }
 function drawSquare(x, y, color, size, canvas) {
     canvas.fillStyle = color
@@ -92,17 +139,17 @@ function returnLineFromVector(x0, y0, x1, y1, material) {
     return (data)
 }
 
-function makeLine(startX,startY,endX,endY,material) {
-    const drawData = returnLineFromVector(startX, startY, endX, endY,material)
+function makeLine(startX, startY, endX, endY, material) {
+    const drawData = returnLineFromVector(startX, startY, endX, endY, material)
     mapData.push(drawData)
     for (let i = 0; i < drawData.length; i++) {
         drawSquare(drawData[i].x, drawData[i].y, material, 1, ctm);
 
     }
 }
-makeLine(400,400,100,100,"blue")
-makeLine(100,100,400,100,"red")
-makeLine(400,100,400,400,"green")
+makeLine(400, 400, 100, 100, "blue")
+makeLine(100, 100, 400, 100, "red")
+makeLine(400, 100, 400, 400, "green")
 
 /*const cuboid1 = new Cuboid([0,0,0],2,2,2)
 objectList.push(cuboid1)
@@ -115,4 +162,7 @@ function Cuboid(originPoint, sizex, sizey, sizez) {
 
 function toRadians(angle) {
     return angle * (Math.PI / 180);
+}
+function calculateVectorDisplacement(angle, magnitude) {
+    return { x: -magnitude * Math.cos(toRadians(angle)), y: -magnitude * Math.sin(toRadians(angle)) }
 }
