5b83828743c988b8649b9652c11b24e20e0f501b
EEEGuba <grzes_pta@o2.pl>
2024-04-30 02:07:27 +0200

    physics tweak since i was on it

--- funny physics thingie/script.js
+++ funny physics thingie/script.js
@@ -1,7 +1,7 @@
 const simulationSpeed = 25 //ms per tick
-const gravity = 0.7 //px per tick
+const gravity = 0.07 //px per tick
 const airResistance = 0.01
-const bounceLoss = 0.9 //1 is no energy lost 0 is no bounce
+const bounceLoss = 0.95 //1 is no energy lost 0 is no bounce
 
 const canvas = document.getElementById("content");
 canvas.width = 500;
@@ -15,7 +15,7 @@ let testSquare = {
     positionx: 50,
     positiony: 50,
     size: 50,
-    color: "red",
+    color: "green",
     wasInCollisionLastFrame:false
 }
 
@@ -43,15 +43,23 @@ function physicsSimulation(object) {
     if(!object.wasInCollisionLastFrame){
     if (object.positionx + object.size > canvas.width) {
         object.vector[0] *= -bounceLoss
+        object.vector[1] *= bounceLoss
         object.positionx = canvas.width-object.size
     }
     if(object.positionx < 0){
         object.vector[0] *= -bounceLoss
+        object.vector[1] *= bounceLoss
         object.positionx = 0
     }
     if (object.positiony + object.size > canvas.height) {
+        object.vector[0] *= bounceLoss
         object.vector[1] *= -bounceLoss
         object.positiony=canvas.height-object.size
+    }
+    if(object.positiony < 0){
+        object.vector[0] *= bounceLoss
+        object.vector[1] *= -bounceLoss
+        object.positiony = 0
     }
     object.wasInCollisionLastFrame=true
 }
