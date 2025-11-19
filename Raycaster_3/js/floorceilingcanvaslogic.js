4e73d2caaadbfea142cda6906217ab0a087aebc3
EEEGuba <grzes_pta@o2.pl>
2025-11-19 23:13:28 +0100

    b

--- Raycaster_3/js/floorceilingcanvaslogic.js
+++ Raycaster_3/js/floorceilingcanvaslogic.js
@@ -1,6 +1,9 @@
+//context.setTransform(Xscale, Xskew, Yskew, Yscale, xMove, Ymove)
+
+ctf.setTransform(1,0, 0, 1, myFloorCanvas.width/2, myFloorCanvas.height/2);
 for (let index = 300; index>0; index--) {
     (function(ind) {
-       setTimeout(()=>{ctf.rotate(20 * Math.PI / 180);ctf.setTransform(1, 0.5, -0.5, 1, 0, index);ctf.drawImage(testTexture1, -20, -20, testTexture1.width, testTexture1.height);
+       setTimeout(()=>{ctf.rotate(20 * Math.PI / 180);ctf.drawImage(testTexture1, 0, 0, testTexture1.width, testTexture1.height);
 ;}, 100 + (30 * ind));
        
    })(index);
