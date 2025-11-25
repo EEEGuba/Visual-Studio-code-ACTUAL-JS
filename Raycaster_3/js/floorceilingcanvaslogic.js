9a24356d0bddd9734dfa34c208c69ecf7ef05c4b
EEEGuba <grzes_pta@o2.pl>
2025-11-25 18:24:41 +0100

    first pseudo-floor is working

--- Raycaster_3/js/floorceilingcanvaslogic.js
+++ Raycaster_3/js/floorceilingcanvaslogic.js
@@ -3,41 +3,48 @@ let DEBUGLOGGERKEY = false
 function drawEntireFrameOfFloors() {
    ctf.resetTransform()
    ctf.clearRect(0, 0, myFloorCanvas.width, myFloorCanvas.height)
+   ctf.setTransform(1, 0, 0, 1,myFloorCanvas.width/2-(mapData.playerData.location.x-500),myFloorCanvas.height/2-(mapData.playerData.location.y-500))
    const floorStartDistance = 100
    const floorEndDistance = 200
    //ctf.drawImage(cobblestone,0,0)
-   const rightNearPoint = calculateVectorDisplacement(angleCorrector(mapData.playerData.azimuth + fov / 2), floorStartDistance)
-   const leftNearPoint = calculateVectorDisplacement(angleCorrector(mapData.playerData.azimuth - fov / 2), floorStartDistance)
-   const rightFarPoint = calculateVectorDisplacement(angleCorrector(mapData.playerData.azimuth + fov / 2), floorEndDistance)
-   const leftFarPoint = calculateVectorDisplacement(angleCorrector(mapData.playerData.azimuth - fov / 2), floorEndDistance)
+   const rightNearPoint = calculateVectorDisplacement(angleCorrector(90 + fov / 2), floorStartDistance)
+   const leftNearPoint = calculateVectorDisplacement(angleCorrector(90 - fov / 2), floorStartDistance)
+   const rightFarPoint = calculateVectorDisplacement(angleCorrector(90 + fov / 2), floorEndDistance)
+   const leftFarPoint = calculateVectorDisplacement(angleCorrector(90 - fov / 2), floorEndDistance)
    const imageWidthNear = -2 * calculateVectorDisplacement(angleCorrector(90 - (fov / 2)), floorEndDistance-floorStartDistance).x
    const imageWidthFar = imageWidthNear * 2
    //ctf.setTransform(1,0,0,1,-rightFarPoint.x,-rightFarPoint.y)
-   //ctf.translate(leftFarPoint.x,leftFarPoint.y)
-   const rectifiedCoordinates = addtwo2DVectors(calculateClockwiseRotatedCoordinatesByXYandAngle(mapData.playerData.location, angleCorrector((-mapData.playerData.azimuth)+90)),{x:myFloorCanvas.width/2,y:myFloorCanvas.height  /2})   
-   ctf.setTransform(1, 0, 0, 1,-rectifiedCoordinates.x,-rectifiedCoordinates.y)
+   const rotationVector = {x:mapData.playerData.location.x-500,y:mapData.playerData.location.y-500}
+   ctf.translate(rotationVector.x,rotationVector.y)
+   //const rectifiedCoordinates = addtwo2DVectors(calculateClockwiseRotatedCoordinatesByXYandAngle(mapData.playerData.location, angleCorrector((-mapData.playerData.azimuth)+90)),{x:myFloorCanvas.width/2,y:myFloorCanvas.height  /2})   
    ctf.rotate(toRadians(angleCorrector(-mapData.playerData.azimuth + 90)))
-   ctf.fillRect(rectifiedCoordinates.x, rectifiedCoordinates.y, 10, 10)
-   ctf.translate(-rectifiedCoordinates.x, -rectifiedCoordinates.y)
+   ctx.fillRect(myFloorCanvas.width/2-5,myFloorCanvas.height/2-5, 10, 10)
+   //ctf.translate(-rectifiedCoordinates.x, -rectifiedCoordinates.y)
+   ctf.translate(-rotationVector.x,-rotationVector.y)
    ctf.fillStyle = "black"
-   for (let linex = 0; linex < 1000; linex++) {
-      ctf.fillRect(linex * 10, 0, 1, 1000)
+   for (let linex = -500; linex < 1000; linex++) {
+      ctf.fillRect(linex * 10, -500, 1, 1000)
    }
    ctf.fillStyle = "brown"
-   for (let liney = 0; liney < 100; liney++) {
-      ctf.fillRect(0, liney * 10, 1000, 1)
+   for (let liney = -500; liney < 1000; liney++) {
+      ctf.fillRect(-500, liney * 10, 1000, 1)
    }
-   //ctf.fillStyle = "red"
-   //ctf.fillRect(-5, -5, 10, 10)
-   //ctf.beginPath();
-   //ctf.moveTo(rightNearPoint.x + mapData.playerData.location.x, rightNearPoint.y + mapData.playerData.location.y);
-   //ctf.lineTo(leftNearPoint.x + mapData.playerData.location.x, leftNearPoint.y + mapData.playerData.location.y);
-   //ctf.lineTo(leftFarPoint.x + mapData.playerData.location.x, leftFarPoint.y + mapData.playerData.location.y);
-   //ctf.lineTo(rightFarPoint.x + mapData.playerData.location.x, rightFarPoint.y + mapData.playerData.location.y);
-   //ctf.lineTo(rightNearPoint.x + mapData.playerData.location.x, rightNearPoint.y + mapData.playerData.location.y);
-   //ctf.stroke();
-   const imageData = ctf.getImageData(rectifiedCoordinates.x, rectifiedCoordinates.y, imageWidthFar, floorEndDistance - floorStartDistance);
-   ctx.putImageData(imageData, 0, myCanvas.height / 2);
+   ctx.fillStyle = "red"
+   ctx.fillRect(-5, -5, 10, 10)
+   ctx.beginPath();
+   ctx.moveTo(rightNearPoint.x + myFloorCanvas.width/2, rightNearPoint.y + myFloorCanvas.height/2);
+   ctx.lineTo(leftNearPoint.x + myFloorCanvas.width/2, leftNearPoint.y + myFloorCanvas.height/2);
+   ctx.lineTo(leftFarPoint.x + myFloorCanvas.width/2, leftFarPoint.y + myFloorCanvas.height/2);
+   ctx.lineTo(rightFarPoint.x + myFloorCanvas.width/2, rightFarPoint.y + myFloorCanvas.height/2);
+   ctx.lineTo(rightNearPoint.x + myFloorCanvas.width/2, rightNearPoint.y + myFloorCanvas.height/2);
+   ctx.stroke();
+   const offScreenCanvas = new OffscreenCanvas(imageWidthFar, floorEndDistance - floorStartDistance)
+   const osc = offScreenCanvas.getContext('2d')
+   const imageData = ctf.getImageData(myFloorCanvas.width/2+leftFarPoint.x,myFloorCanvas.height/2+leftFarPoint.y, imageWidthFar, floorEndDistance - floorStartDistance);    
+   osc.putImageData(imageData,0,0)
+   ctx.drawImage(offScreenCanvas, 0,300,1200,300)
+
+   
 }
 
 //image rotation is 360-player azimuth!
